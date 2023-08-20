f1dece6106b71dee96da19582c8ff53d0b69ce08
rkibistu <ogreanrazvan@gmail.com>
2023-08-20 13:20:08 +0300

    changed url for bucket again

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,7 +140,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('STATIC_SECRET_KEY')
 
 AWS_STORAGE_BUCKET_NAME = os.getenv('STATIC_BUCKET_NAME')
 #AWS_S3_ENDPOINT_URL = os.getenv('STATIC_ENDPOINT_URL')
-AWS_S3_ENDPOINT_URL = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -149,7 +149,7 @@ AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = 'https://{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATIC_ROOT = 'static/'
 
 
