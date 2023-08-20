3772732343f24b517a2a309c52aeadb817194206
rkibistu <ogreanrazvan@gmail.com>
2023-08-20 13:18:12 +0300

    changed bucket url

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,7 +149,7 @@ AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = 'https://{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATIC_ROOT = 'static/'
 
 
